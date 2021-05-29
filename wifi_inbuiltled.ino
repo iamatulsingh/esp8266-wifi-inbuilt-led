adaa6d27f4f7852a06c9434fe86a8547929e56cd
Atul Singh <atulsingh0401@gmail.com>
2021-05-29 11:01:08 +0530

    Add files via upload

---
+++ wifi_inbuiltled.ino
@@ -0,0 +1,100 @@
+#include <ESP8266WiFi.h>
+ 
+const char* ssid = "ACTFIBERNET";
+const char* password = "9148596891atko1234";
+ 
+int ledPin = 2;
+WiFiServer server(80);
+ 
+void setup() {
+  Serial.begin(9600);
+  delay(10);
+ 
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+ 
+  // Connect to WiFi network
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+ 
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+ 
+  // Start the server
+  server.begin();
+  Serial.println("Server started");
+ 
+  // Print the IP address
+  Serial.print("Use this URL to connect: ");
+  Serial.print("http://");
+  Serial.print(WiFi.localIP());
+  Serial.println("/");
+ 
+}
+ 
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+ 
+  // Wait until the client sends some data
+  Serial.println("new client");
+  while(!client.available()){
+    delay(1);
+  }
+ 
+  // Read the first line of the request
+  String request = client.readStringUntil('\r');
+  Serial.println(request);
+  client.flush();
+ 
+  // Match the request
+ 
+  int value = LOW;
+  if (request.indexOf("/LED=ON") != -1)  {
+    digitalWrite(ledPin, LOW);
+    value = LOW;
+  }
+  if (request.indexOf("/LED=OFF") != -1)  {
+    digitalWrite(ledPin, HIGH);
+    value = HIGH;
+  }
+ 
+// Set ledPin according to the request
+//digitalWrite(ledPin, value);
+ 
+  // Return the response
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println(""); //  do not forget this one
+  client.println("<!DOCTYPE HTML>");
+  client.println("<html>");
+  client.println("<head><title>ESP8266</title><link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css'></head>");
+  client.println("<div class='row mt-4'><div class='col-sm-4'></div><div class='col-sm-4'><div class='card text-center'><div class='card-header'>NODEMCU ESP8266</div><div class='card-body'>");
+  client.print("Led pin is now: ");
+ 
+  if(value == HIGH) {
+    client.print("<b>Off</b>");
+  } else {
+    client.print("<b>On</b>");
+  }
+  client.println("<br><br>");
+  client.println("<a class='btn btn-primary' href=\"/LED=ON\"\">Turn On</a>");
+  client.println("<a class='btn btn-warning' href=\"/LED=OFF\"\">Turn Off</a><br/>");
+  client.println("</div><div class='card-footer text-muted'>DEMO</div></div></div></div>");
+  client.println("</html>");
+ 
+  delay(1);
+  Serial.println("Client disonnected");
+  Serial.println("");
+}
